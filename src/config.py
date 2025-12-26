fca31b44fd4699167ea69b79ba81c732416730a4
subharatti <subharatti@gmail.com>
2025-12-26 03:42:02 -0500

    Removed not needed files

--- src/config.py
+++
@@ -1,8 +0,0 @@
-DATA_FILE = "data/transactions.json"
-
-BUDGET_LIMITS = {
-    "Food": 300,
-    "Entertainment": 100,
-    "Transportation": 150,
-    "Housing": 1200
-}
--- src/reports.py
+++
@@ -1,9 +0,0 @@
-def print_summary(expenses, income_total):
-    print("\nSpending Summary\n")
-
-    for category, total in expenses.items():
-        print(f"{category:<18}: ${total:.2f}")
-
-    if income_total > 0:
-        print("\nIncome\n")
-        print(f"{'Total Income':<18}: ${income_total:.2f}")
